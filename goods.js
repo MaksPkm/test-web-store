4dcae5e2fb22a6e74f3724494bbb5348a99af487
Maksym <maksymkoshelap@gmail.com>
2024-11-15 14:39:11 +0100

    add next

---
+++ goods.js
@@ -0,0 +1,16 @@
+const GOODS = [{
+    id: 1,
+    name: 'Coworkers',
+    price: 100,
+    imgSrc: 'images/goods/coworkers.jpg',
+}, {
+    id: 2,
+    name: 'Analytics',
+    price: 200,
+    imgSrc: 'images/goods/analytics.jpg',
+}, {
+    id: 3,
+    name: 'Rent office',
+    price: 500,
+    imgSrc: 'images/goods/rent-place.jpg',
+}];
